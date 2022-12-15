2851eafffeb264435da3c6f75d00694bf9171cd9
A-Szydlowska <235992@edu.p.lodz.pl>
2022-12-15 18:08:43 +0100

    DB_scan changed for two series of data

--- backend/DB_scan.py
+++ backend/DB_scan.py
@@ -3,34 +3,46 @@ from copy import deepcopy
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 
+# returning list because now we have two series
 
-def db_scan(data: pd.arrays, target: str, date: str, multiplayer=0.5) -> pd.arrays:
+
+def db_scan(data1: pd.arrays, data2: pd.arrays, target: str, date: str, multiplayer=0.5) -> list:
     multiplayer = 1 - multiplayer + 0.01
-    epsilon = 16 / data.count().values[0]
+    epsilon = 16 / data1.count().values[0]
     epsilon *= multiplayer
-    ad_data = deepcopy(data)
 
-    target_col = ad_data[[target]].copy()
+    # Check if data2 is not None (possibly important)
+    if data2 is not None:
+        ad_datas = [deepcopy(data1), deepcopy(data2)]
+        target_cols = [ad_datas[0][[target]].copy(), ad_datas[1][[target]].copy()]
+        array_length = 2
+    else:
+        ad_datas = [deepcopy(data1)]
+        target_cols = [ad_datas[0][[target]].copy()]
+        array_length = 1
 
-    scaler = StandardScaler()
-    np_scaled = scaler.fit_transform(target_col.values.reshape(-1, 1))
-    data_scaled = pd.DataFrame(np_scaled)
+    # loop to get two series of db_scan
+    for i in range(0, array_length):
 
-    dbscan = DBSCAN(eps=epsilon, min_samples=5)
+        scaler = StandardScaler()
+        np_scaled = scaler.fit_transform(target_cols[i].values.reshape(-1, 1))
+        data_scaled = pd.DataFrame(np_scaled)
 
-    dbscan.fit(data_scaled)
-    target_col['Anomaly_after_method'] = dbscan.fit_predict(data_scaled)
+        dbscan = DBSCAN(eps=epsilon, min_samples=5)
 
-    target_col.loc[target_col['Anomaly_after_method'] != -1, 'Anomaly'] = False
-    target_col.loc[target_col['Anomaly_after_method'] == -1, 'Anomaly'] = True
+        dbscan.fit(data_scaled)
+        target_cols[i]['Anomaly_after_method'] = dbscan.fit_predict(data_scaled)
 
-    target_col['Date'] = data[date]
-    target_col = target_col.rename(columns={target: "Exchange"})
+        target_cols[i].loc[target_cols[i]['Anomaly_after_method'] != -1, 'Anomaly'] = False
+        target_cols[i].loc[target_cols[i]['Anomaly_after_method'] == -1, 'Anomaly'] = True
 
-    target_col = target_col.drop('Anomaly_after_method', axis=1)
+        target_cols[i]['Date'] = ad_datas[i][date]
+        target_cols[i] = target_cols[i].rename(columns={target: "Exchange"})
 
-    target_col.insert(0, 'Date', target_col.pop('Date'))
-    target_col.insert(1, 'Exchange', target_col.pop('Exchange'))
-    target_col.insert(2, 'Anomaly', target_col.pop('Anomaly'))
+        target_cols[i] = target_cols[i].drop('Anomaly_after_method', axis=1)
 
-    return target_col
+        target_cols[i].insert(0, 'Date', target_cols[i].pop('Date'))
+        target_cols[i].insert(1, 'Exchange', target_cols[i].pop('Exchange'))
+        target_cols[i].insert(2, 'Anomaly', target_cols[i].pop('Anomaly'))
+
+    return target_cols
