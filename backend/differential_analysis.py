3fa5068fda411448b22734e548c35c8c03e39842
A-Szydlowska <235992@edu.p.lodz.pl>
2022-12-22 21:30:55 +0100

    Differential_analysis changes

--- backend/differential_analysis.py
+++ backend/differential_analysis.py
@@ -7,18 +7,25 @@ from backend.backend_functions import run_method
 # differential analysis (to check)
 
 
-def differential_analysis(data1: pd.arrays = None, data2: pd.arrays = None, target: str = None, method: str = None,
-                          date: str = None, parameter=0):
-    ad_datas = [deepcopy(data1), deepcopy(data2)]
-    targets_test = [ad_datas[0][[target]].copy(), ad_datas[1][[target]].copy()]
+def differential_analysis(datas: list, target: str = None, method: str = None, date: str = None, parameter=0):
+    if datas is None:
+        return
+
+    ad_datas = list([deepcopy(i) for i in datas if i is not None])
+
+    if len(ad_datas) < 2:
+        return
+
+    targets_test = list([data[[target]].copy() for data in ad_datas])
+
     differ = pd.DataFrame()
-    differ["Data"] = ad_datas[0][date]
+    differ[date] = ad_datas[0][date]
 
     scaler = MinMaxScaler(feature_range=(-1, 1))
     d1 = scaler.fit_transform(targets_test[0][[target]])
     d2 = scaler.fit_transform(targets_test[1][[target]])
 
     differ[target] = d1 - d2
-    difference = run_method(differ, target, date, method, parameter)
+    difference = run_method([differ], target, date, method, parameter)
 
     return difference
