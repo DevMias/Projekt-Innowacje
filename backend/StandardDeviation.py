8186667b88dbc1e456bcea0916f54657be5d7018
DevMias <jakub.mijas2001@gmail.com>
2023-01-05 00:31:15 +0100

    Zmiany w Odchyleniu Standardowym

--- backend/StandardDeviation.py
+++ backend/StandardDeviation.py
@@ -3,27 +3,43 @@ from copy import deepcopy
 import numpy as np
 
 
-def standard_deviation(data: pd.arrays, target: str, date: str) -> pd.arrays:
-    ad_data = deepcopy(data)
-    target_col = ad_data[[target]].copy()
+def standard_deviation(datas: list, target: str, date: str) -> pd.arrays:
+    
+    if datas is None:
+        return
+    
+    ad_datas = list([deepcopy(i) for i in datas if i is not None])
 
-    mean = np.mean(target_col[target])
-    sd = np.std(target_col[target])
+    if len(ad_datas) == 0:
+        return
 
-    temp_anomalies = []
 
-    for idx, x in enumerate(target_col[target]):
-        if x < mean - sd or x > mean + sd:
-            temp_anomalies.append(True)
+    target_cols = list([data[[date, target]].copy() for data in ad_datas])
+
+    means = []
+    sds = []
+    for target_col in target_cols:
+        means.append(np.mean(target_col[target]))
+        sds.append(np.std(target_col[target]))
+
+    temp_anomalies = [[] for _ in range(target_cols[target])]
+
+    for i, target_col in enumerate(target_cols[target]):
+        if target_col < means[i] - sds[i] or target_col > means[i] + sds[i]:
+            temp_anomalies[i].append(True)
         else:
-            temp_anomalies.append(False)
+            temp_anomalies[i].append(False)
 
     target_col['Anomaly'] = temp_anomalies
     target_col['Date'] = data[date]
     target_col = target_col.rename(columns={target: "Exchange"})
 
-    target_col.insert(0, 'Date', target_col.pop('Date'))
-    target_col.insert(1, 'Exchange', target_col.pop('Exchange'))
+    target_cols[i].rename(columns={date: "Date"}, inplace=True)
+    target_cols[i].rename(columns={target: "Exchange"}, inplace=True)
     target_col.insert(2, 'Anomaly', target_col.pop('Anomaly'))
 
-    return target_col
+  
+    if len(target_cols) == 1:
+        return target_cols[0]
+    else:
+        return target_cols
