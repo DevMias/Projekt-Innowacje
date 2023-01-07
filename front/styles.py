0122b57bf35fec4a7f41d833e5a44d39b022af9f
jaspero <kacper.devek@gmail.com>
2023-01-07 21:59:39 +0100

    Dodanie zmiennych do stylowania

--- front/styles.py
+++ front/styles.py
@@ -1,107 +1,113 @@
-buttonStyleSheet = """
-QPushButton { 
+font_color = 'red'
+hover_color = 'rgb(28, 185, 199)'
+gray_color = 'rgb(75, 78, 82)'
+font_size_20 = '20px'
+font_size_30 = '30px'
+font_size_15 = '15px'
+buttonStyleSheet = f"""
+QPushButton {{ 
     background-color: rgb(75, 78, 82);
-    color: white;
+    color: {font_color};
     padding: 2px;
-    font: 20px;
+    font: {font_size_20};
     border: 1px inset rgb(24, 26, 25);
     border-radius: 10px;
     width:60px;
-}
-QPushButton:hover {
-    background-color: rgb(28, 185, 199);
-}
-#year_checkbox{
-    font-size:26px;
-    color: white;
+}}
+QPushButton:hover {{
+    background-color: {hover_color};
+}}
+#year_checkbox{{
+    font-size:{font_size_20};
+    color: {font_color};
     text-transform: uppercase;
     font-family: "Calibri", Times, serif;
-}
+}}
 """
 
-swapButtonStyleSheet = """
-QPushButton {
-    background-color: rgb(75, 78, 82);
-    color: white;
+swapButtonStyleSheet = f"""
+QPushButton {{
+    background-color: {gray_color};
+    color:{font_color};
     padding: 15px;
-    font: 20px;
+    font: {font_size_20};
     border: 1px inset rgb(24, 26, 25);
     border-radius: 10px;
-}
-QPushButton:hover {
-    background-color: rgb(28, 185, 199);
-}
+}}
+QPushButton:hover {{
+    background-color: {hover_color};
+}}
 """
 
-generatePlotButtonStyleSheet = """
-QPushButton { 
-    background-color: rgb(75, 78, 82);
-    color: white;
+generatePlotButtonStyleSheet = f"""
+QPushButton {{ 
+    background-color: {gray_color};
+    color:{font_color};
     padding: 10px;
-    font: bold 20px;
+    font: bold {font_size_20};
     border: 1px inset rgb(24, 26, 25);
     border-radius: 10px;
-}
-QPushButton:hover {
-    background-color: rgb(28, 185, 199);
-}
+}}
+QPushButton:hover {{
+    background-color: {hover_color};
+}}
 """
 
-DateEditStyleSheet = """
+DateEditStyleSheet = f"""
 QDateEdit
-{
-    color: white;
+{{
+    color:{font_color};
     border-radius: 3px;
     border: 1px solid rgb(24, 26, 25);
-    font: 18px;
+    font: {font_size_20};
     background-color:rgb(75, 78, 82);
     text-align:center;
     margin-left: 5px;
     padding:4px;
-}
+}}
 
-#settings_element{
+#settings_element{{
     max-width:600px;
     min-width:400px;
     text-align:center;
-    background-color:rgb(75, 78, 82);
+    background-color:{gray_color};
     border: 1px solid rgb(24, 26, 25);
-}
-    
+}}
+
 QDateEdit::down-arrow
-{
+{{
     image: url(icons/arrow_down.png);
-}
-QDateEdit::drop-down {
+}}
+QDateEdit::drop-down {{
     width: 28px;
-}
+}}
 """
 
-calendarStyleSheet = """
-QCalendarWidget QWidget{
+calendarStyleSheet = f"""
+QCalendarWidget QWidget{{
     alternate-background-color: #6e6e70;
     color: #fbfbfb;
-}
-QCalendarWidget QToolButton#qt_calendar_prevmonth{
+}}
+QCalendarWidget QToolButton#qt_calendar_prevmonth{{
     qproperty-icon: url(icons/left_arrow.png);
-}
-QCalendarWidget QToolButton#qt_calendar_nextmonth{
+}}
+QCalendarWidget QToolButton#qt_calendar_nextmonth{{
     qproperty-icon: url(icons/right_arrow.png);
-}
-QCalendarWidget QToolButton:hover{
-    background-color: rgb(28, 185, 199);
-}
-QCalendarWidget QAbstractItemView:enabled{
+}}
+QCalendarWidget QToolButton:hover{{
+    background-color: {hover_color};
+}}
+QCalendarWidget QAbstractItemView:enabled{{
     background-color: #212121;
     color: #fbfbfb;
     selection-background-color: rgb(28, 185, 199);
     selection-color: green;
-}
+}}
 """
 
-comboBoxStyleSheet = """
-QComboBox {
-    color: white;
+comboBoxStyleSheet = f"""
+QComboBox {{
+    color:{font_color};
     border: 1px solid rgb(24, 26, 25);
     padding: 1px 18px 1px 3px;
     min-width: 6em;
@@ -109,142 +115,138 @@ QComboBox {
     background-color: rgb(75, 78, 82);
     margin-left: 5px;
     padding:4px;
-}
-QComboBox QAbstractItemView{ /* Stylowanie drop-down menu */
-    color:white;
-}
-#settings_element{
+}}
+QComboBox QAbstractItemView{{ /* Stylowanie drop-down menu */
+    color:{font_color};
+}}
+#settings_element{{
     max-width:500px;
     min-width:500px;
     text-align:center;  
     border: 1px solid rgb(24, 26, 25);
-    background-color:rgb(75, 78, 82);
-}
-QComboBox::down-arrow {
+    background-color:{gray_color};
+}}
+QComboBox::down-arrow {{
     image: url(icons/arrow_down.png);
-}
-QComboBox::drop-down {
+}}
+QComboBox::drop-down {{
     width: 28px;
-}
+}}
 """
 
-labelStyleSheet = """
-QLabel {
+labelStyleSheet = f"""
+QLabel {{
     border: 0;
     padding: 2px;
-    font: bold 22px;
+    font: bold {font_color};
     text-transform:uppercase;
-    color:white;
-}
-QLabel#graph_title {
+    color:{font_color};
+}}
+QLabel#graph_title {{
     margin-top: 20px;
-    color:white;
-}
-#graph_fields{
+    color:{font_color};
+}}
+#graph_fields{{
     margin: 0 auto;
     text-align: right;
-}
-QLineEdit {
-    color:white;
+}}
+
+QLineEdit {{
+    color:{font_color};
     padding: 8px;
     border: 0;
     border-bottom: 1px solid rgb(169, 167, 169);
     background-color:rgb(67, 66, 67);
-    font: 15px;
-}
-#default_label{
+    font: {font_size_15}
+}}
+#default_label{{
     margin-left:200px; /* label z settingsow */
-}
+}}
 """
 
-labelStyleSheet_red = """
-QLabel {
+labelStyleSheet_red = f"""
+QLabel {{
     padding: 2px;
-    font: bold 22px;
-    text-transform:uppercase;
-    color: red;
-}
+    font: bold {font_size_15};
+    color:{font_color};
+}}
 """
 
-labelStyleSheet_orange = """
-QLabel {
+labelStyleSheet_orange = f"""
+QLabel {{
     padding: 2px;
-    font: bold 22px;
-    text-transform:uppercase;
+    font: bold {font_size_15};
     color: orange;
-}
+}}
 """
 
-labelStyleSheet_yellow = """
-QLabel {
+labelStyleSheet_yellow = f"""
+QLabel {{
     padding: 2px;
-    font: bold 22px;
-    text-transform:uppercase;
+    font: bold {font_size_15};
     color: #FEE227;
-}
+}}
 """
 
-labelStyleSheet_light_green = """
-QLabel {
+labelStyleSheet_light_green = f"""
+QLabel {{
     padding: 2px;
-    font: bold 22px;
-    text-transform:uppercase;
+    font: bold {font_size_15};
     color: lightGreen;
-}
+}}
 """
 
-settingsLayoutStyleSheet = """
-    QPushButton{
+settingsLayoutStyleSheet = f"""
+QPushButton{{
     width:64px;
-    }
+}}
 """
 
-labelStyleSheet_green = """
-QLabel {
+labelStyleSheet_green = f"""
+QLabel {{
     padding: 2px;
-    font: bold 22px;
-    text-transform:uppercase;
+    font: bold {font_size_15};
     color: green;
-}
+}}
 """
 
-labelStyleSheet_big = """
-QLabel {
-    color:white;
+labelStyleSheet_big = f"""
+QLabel {{
+    color:{font_color};
     padding: 2px;
-    font: bold 30px;
-}
+    font: bold {font_size_30};
+}}
 """
 
-labelStyleSheet_not_bold = """
-QLabel {
-    color:white;
+labelStyleSheet_not_bold = f"""
+QLabel {{
+    color:{font_color};
     padding: 2px;
-    font: 15px;
-}
+    font: {font_size_15};
+}}
 """
 
-mainTabStyleSheet = """
-    QWidget {
+mainTabStyleSheet = f"""
+    QWidget {{
         background-color: #393939;
-    }
-    QTabWidget{
+    }}
+    QTabWidget{{
         border:2px solid #939393;
         min-width:80px;
         height:18px;
         padding:20px;
         font-weight:bold;
-        font-size:16px;
-    }
-    QTabWidget::pane{
+        font-size:{font_size_15};
+    }}
+    QTabWidget::pane{{
         border: none; /* tu byl ten border zepsuty*/
-    }
-    QTabBar::tab{
-        color:white;
+    }}
+    QTabBar::tab{{
+        color:{font_color};
         background-color:rgb(82, 82, 82);
         font-weight:normal;
-    }
-    QTabBar::tab:selected { 
+    }}
+    QTabBar::tab:selected {{ 
         color:black;
         background-color: rgb(242, 242, 242);
         min-width:80px;
@@ -252,25 +254,24 @@ mainTabStyleSheet = """
         padding:10px;
         font-weight:bold;
         font-size:16px;
-    }
+    }}
 """
 
-windowStyleSheet = """
-QWidget {
-    color:white;
+windowStyleSheet = f"""
+QWidget {{
+    color:{font_color};
     background-color: #323232;
     font-family: "Calibri", Times, serif;
-}
-QWidget:hover{
+}}
+QWidget:hover{{
     color:black;
-}
-QLabel{
+}}
+QLabel{{
     border:0;
     padding: 10px;
     font-family: "Calibri", Times, serif;
-}
+}}
 """
-
 
 swap_icon = "icons/swap.png"
 
